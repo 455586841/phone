e3302be3bb18a706435364d4e35a5ad6fb9ae890
Soar、毅 <Soar360@live.com>
2016-09-24 17:49:25 +0800

    增加显示字段

--- java/phone/src/main/java/com/coderbusy/phone/App.java
+++ java/phone/src/main/java/com/coderbusy/phone/App.java
@@ -14,9 +14,14 @@ public class App {
 		PhoneDataReader reader = new PhoneDataReader(new RandomAccessFile(dataFilePath, "r"));
 		PhoneData data = reader.search("1851105");
 		if(data!=null){
-			System.out.println("运营商:"+data.getTelecomOperator());
+			System.out.println("集团:"+data.getCorp());
 			System.out.println("省份:"+data.getProvince());
 			System.out.println("城市:"+data.getCity());
+			System.out.println("区号:"+data.getAreaCode());
+			System.out.println("邮编:"+data.getPostCode());
+			System.out.println("运营商:"+data.getTelecomOperator());
+			System.out.println("虚拟运营商:"+data.getVirtualNetworkOperator());
+			System.out.println("卡类型:"+data.getCard());
 		}else{
 			System.out.println("数据未找到");
 		}
